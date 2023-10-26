bcef9bce823c88a052834ac06ecf42607d7c49de
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2023-10-26 13:09:13 -0400

    update conversion

--- src/app/data/import/json-to-obj.ts
+++ src/app/data/import/json-to-obj.ts
@@ -11,91 +11,85 @@ import { ElemType, ELEMENTS } from 'src/app/shared/types/dataTypes';
 import { Monster, MonsterAction, MonsterActionCardIcons } from 'src/app/pages/game/models/monster/monster.model';
 
 const getElemType = (text: string): ElemType => {
-    return ELEMENTS.find(e => e.toString() as ElemType === text) as ElemType;
+  return ELEMENTS.find(e => e.toString() as ElemType === text) as ElemType;
 };
 
 export const loadMonsters = (selectedMonster?: any): Array<Monster> => {
-    let ALL = [];
-    if (selectedMonster) {
-        ALL = [selectedMonster];
-    } else {
-        ALL = [
-            Chargroar,
-            Drownigator,
-            Flexferno,
-            Galeaffy,
-            Stalagrowth,
-            Phantomaton,
-            Vulturock,
-            Willard,
-            Zappguin,
-        ];
-    }
-    return convertFromJSON(ALL);
+  let ALL = [];
+  if (selectedMonster) {
+    ALL = [selectedMonster];
+  } else {
+    ALL = [
+      Chargroar,
+      Drownigator,
+      Flexferno,
+      Galeaffy,
+      Stalagrowth,
+      Phantomaton,
+      Vulturock,
+      Willard,
+      Zappguin,
+    ];
+  }
+  return convertFromJSON(ALL);
 };
 
 export const convertFromJSON = (all: Array<any>): Array<Monster> => {
-    let out = new Array<Monster>();
-    all.forEach(json => {
-        const monster: Monster = new Monster();
-        let MONSTER_PROPERTIES = [
-            'name',
-            'text',
-            'hp',
-            'initiative',
-        ];
-        //@ts-ignore
-        MONSTER_PROPERTIES.forEach(p => monster[p] = json[p]);
-        const elements = Array<ElemType>();
-        json.elements.forEach((element: string) => elements.push(getElemType(element)));
-        monster.elements = elements;
-        monster.actions = new Array<MonsterAction>();
-        const ACTIONS = 4;
-        let ACTION_PROPERTIES = [
-            'name',
-            'text',
-            'attack',
-            'speed',
-        ];
-        for (let i = 0; i < ACTIONS; i++) {
-            const action: MonsterAction = new MonsterAction();
-            const jsonAction = json.actions[i];
-            // @ts-ignore
-            ACTION_PROPERTIES.forEach((p) => action[p] = jsonAction[p]);
-            action.element = getElemType(json.actions[i].element);
-            action.isStatus = jsonAction['statusFlg'];
-            action.index = i;
-            action.icons = new MonsterActionCardIcons(); 
-            action.icons.buff = jsonAction.buff;
-            action.icons.discard = jsonAction.discard;
-            action.icons.draw = jsonAction.draw;
-            monster.actions.push(action);
-        }
-        // monster.buffs = new Array<Buff>();
-        // const BUFFS = 4;
-        // let BUFF_PROPERTIES = [
-        //     'buffText',
-        //     'buffName',
-        //     'auraDuration',
-        //     'lastUpdated',
-        // ];
-        // if (keepGUI) {
-        //     BUFF_PROPERTIES = BUFF_PROPERTIES.concat(
-        //         'isSelected',
-        //     )
-        // }
-        // for (let i = 0; i < BUFFS; i++) {
-        //     const buff = new Buff();
-        //     BUFF_PROPERTIES.forEach(p => buff[p] = json.buffs[i][p]);
-        //     buff.monsterName = monster.monsterName;
-        //     monster.buffs.push(buff);
-        // }
-        // out = out.sort((a, b) => {
-        //     if (a.monsterName > b.monsterName) {return 1; }
-        //     if (a.monsterName < b.monsterName) {return -1; }
-        //     return 0;
-        // });
-        out.push(monster as Monster);
-    });
-    return out;
+  let out = new Array<Monster>();
+  all.forEach(json => {
+    const monster: Monster = new Monster(
+      json.name,
+      json.elements,
+      json.text,
+      json.hp,
+      json.initiative,
+      json.actions.map(getMonsterAction),
+    );
+    // monster.buffs = new Array<Buff>();
+    // const BUFFS = 4;
+    // let BUFF_PROPERTIES = [
+    //     'buffText',
+    //     'buffName',
+    //     'auraDuration',
+    //     'lastUpdated',
+    // ];
+    // if (keepGUI) {
+    //     BUFF_PROPERTIES = BUFF_PROPERTIES.concat(
+    //         'isSelected',
+    //     )
+    // }
+    // for (let i = 0; i < BUFFS; i++) {
+    //     const buff = new Buff();
+    //     BUFF_PROPERTIES.forEach(p => buff[p] = json.buffs[i][p]);
+    //     buff.monsterName = monster.monsterName;
+    //     monster.buffs.push(buff);
+    // }
+    // out = out.sort((a, b) => {
+    //     if (a.monsterName > b.monsterName) {return 1; }
+    //     if (a.monsterName < b.monsterName) {return -1; }
+    //     return 0;
+    // });
+    out.push(monster);
+  });
+  return out;
+}
+
+const getMonsterAction = (action: any): MonsterAction => {
+  return new MonsterAction(
+    action.name,
+    action.text,
+    action.attack,
+    action.speed,
+    action.element,
+    getMonsterActionCardIcon(action),
+    0
+  );
+}
+
+const getMonsterActionCardIcon = (action: any): MonsterActionCardIcons => {
+  return new MonsterActionCardIcons(
+    action.buff,
+    action.discard,
+    action.draw
+  );
 }
