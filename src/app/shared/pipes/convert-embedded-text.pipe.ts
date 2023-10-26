b8c5c4d27c5c7f3cdc72530b7f0cbe7cc14c6d7c
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2023-10-25 22:22:41 -0400

    pipedate :3

--- src/app/shared/pipes/convert-embedded-text.pipe.ts
+++ src/app/shared/pipes/convert-embedded-text.pipe.ts
@@ -2,16 +2,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { AbilityTextUtil } from '../utils/ability-text.util';
 import { Css } from '../types/dataTypes';
 
+interface ConvertEmbeddedTextPipeConfig {
+  termCss?: Css,
+  imageCss?: Css,
+  excludeTerms?: boolean,
+};
+
 @Pipe({
   name: 'convertEmbeddedText'
 })
 export class ConvertEmbeddedTextPipe implements PipeTransform {
 
-  transform(value: string, termCss?: Css, imageCss?: Css): string {
-    if (termCss || imageCss) {
-      return AbilityTextUtil.getAbilityText(value, termCss, imageCss);
+  transform(value: string, config?: ConvertEmbeddedTextPipeConfig): string {
+    let text = value;
+    if (config?.excludeTerms) {
+      text = AbilityTextUtil.getAbilityTextWithoutTerms(text);
     }
-    return AbilityTextUtil.getAbilityTextDefaults(value);
+    if (config?.termCss && config?.imageCss) {
+      return AbilityTextUtil.getAbilityText(text, config.termCss, config.imageCss);
+    }
+    return AbilityTextUtil.getAbilityTextDefaults(text);
   }
 
 }
