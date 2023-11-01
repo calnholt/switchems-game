ee9211ebd484c89977588f6f76d96224023b6042
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2023-10-31 21:41:49 -0400

    team aura

--- src/app/pages/game/models/monster/monster.model.ts
+++ src/app/pages/game/models/monster/monster.model.ts
@@ -242,17 +242,19 @@ export class MonsterActionCardIcons {
 }
 
 export class Buff implements IHaveTooltip {
-  private monsterName!: string;
-  private name!: string;
-  private text!: string;
+  private monsterName: string;
+  private name: string;
+  private text: string;
+  private auraDuration: number;
   public isAppliedAsBuff: boolean = false;
   public isAppliedAsDiscard: boolean = false;
   private terms: Term[] = [];
 
-  constructor(monsterName: string, name: string, text: string) {
+  constructor(monsterName: string, name: string, text: string, auraDuration: number) {
     this.monsterName = monsterName;
     this.name = name;
     this.text = text;
+    this.auraDuration = auraDuration;
     this.terms = AbilityTextUtil.getTermsFromText(text);
   }
   hasTooltip(): boolean {
@@ -262,6 +264,7 @@ export class Buff implements IHaveTooltip {
   get _monsterName(): string { return this.monsterName; }
   get _name(): string { return this.name; }
   get _text(): string { return this.text; }
+  get _auraDuration(): number { return this.auraDuration; }
   get _isAppliedAsBuff(): boolean { return this.isAppliedAsBuff; }
   get _isAppliedAsDiscard(): boolean { return this.isAppliedAsDiscard; }
   get _terms(): Term[] { return this.terms; }
