41866bd516face47766d15cb2d8db55318eac3ac
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2023-12-23 22:25:41 -0500

    cleanup

--- src/app/shared/utils/array.util.ts
+++ src/app/shared/utils/array.util.ts
@@ -7,32 +7,32 @@ export const ArrayUtil = {
   getRandomUniqueEntriesFromArray
 };
 
-function getRandomItemFromArray<T>(arr: T[], random: () => number): T | undefined {
+function getRandomItemFromArray<T>(arr: T[]): T | undefined {
   if (arr.length === 0) {
     return undefined;
   }
-  return arr[getRandomIndex(arr.length, random)];
+  return arr[getRandomIndex(arr.length)];
 }
 
-function getRandomIndex(length: number, random: () => number) { return (random() * length); }
+function getRandomIndex(length: number) { return Math.floor(Math.random() * length); }
 
-function randomizeOrder<T>(array: T[], random: () => number) {
+function randomizeOrder<T>(array: T[]) {
   let arrayCopy = [...array];
   for (let i = arrayCopy.length - 1; i > 0; i--) {
-    const j = (random() * (i + 1));
+    const j = Math.floor(Math.random() * (i + 1));
     [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
   }
   return arrayCopy;
 }
 
-function getRandomUniqueEntriesFromArray<T>(array: T[], entries: number, random: () => number): T[] {
+function getRandomUniqueEntriesFromArray<T>(array: T[], entries: number): T[] {
   if (entries <= 0 || entries > array.length) {
     return [];
   }
   const result: T[] = [];
   const indices: number[] = array.map((a: T, i: number) => i);
   for (let i = 0; i < entries; i++) {
-    const randomIndex = getRandomIndex(indices.length, random);
+    const randomIndex = getRandomIndex(indices.length);
     const index = indices[randomIndex];
     result.push(array[index]);
     indices.splice(randomIndex, 1);
