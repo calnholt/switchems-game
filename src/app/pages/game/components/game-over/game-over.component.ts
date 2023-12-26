6b815072c29e2f6d9c38c5c5f824a2ec9c01e621
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2023-12-26 13:16:14 -0500

    implement game over component

--- src/app/pages/game/components/game-over/game-over.component.ts
+++ src/app/pages/game/components/game-over/game-over.component.ts
@@ -2,11 +2,15 @@ import { Component, Input } from '@angular/core';
 import { GameOverService, WinnerType } from '../../services/game-over/game-over.service';
 import { CurrentPhaseService } from '../../services/current-phase/current-phase.service';
 import { ImageUtil } from '~/app/shared/utils/image.util';
+import { slideInLeftAnimation, slideInLeftOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'sw-game-over',
   templateUrl: './game-over.component.html',
-  styleUrls: ['./game-over.component.scss']
+  styleUrls: ['./game-over.component.scss'],
+  animations: [
+    slideInLeftOnEnterAnimation({ translate: '1000px', duration: 300 }),
+  ]
 })
 export class GameOverComponent {
   @Input() winner: WinnerType = null; 
