46a22a01c76b5906ad0741ff6a46f98815f685ff
DESKTOP-8BUND66\Calvin <calnholt@gmail.com>
2023-12-30 13:10:01 -0500

    sfx updates and idle animations

--- src/app/shared/services/sfx.service.ts
+++ src/app/shared/services/sfx.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Path } from '../types/dataTypes';
+import { CurrentPhaseService } from '~/app/pages/game/services/current-phase/current-phase.service';
+import { EventType, Router } from '@angular/router';
 
-const BASE_PATH = '../../../../assets/audio/ui/';
+const UI_PATH = '../../../../assets/audio/ui/';
+const MUSIC_PATH = '../../../../assets/audio/music/';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +14,40 @@ export class SfxService {
 
   private _playSfx: BehaviorSubject<SfxType> = new BehaviorSubject<SfxType>('');
 
+  private music = new Audio();
+  private _pause = false;
+  private _mute = false;
+
   public get playSfx() { return this._playSfx.value; }
   public get playSfx$() { return this._playSfx; }
+  public get paused() { return this._pause; }
+  public get muted() { return this._mute; }
 
-  constructor() { 
+  constructor(
+    private currentPhaseService: CurrentPhaseService,
+    private router: Router,
+  ) { 
     this._playSfx.subscribe((value) => {
       const audio = new Audio(this.getPath(value));
-      audio.play().catch(err => console.error('Error playing sound:', err));
+      if (value.includes('MUSIC')) {
+        this.music = audio;
+        this.music.loop = true;
+        this.music.play().catch(err => console.error('Error playing sound:', err));
+      }
+      else {
+        audio.play().catch(err => console.error('Error playing sound:', err));
+      }
+    });
+    this.currentPhaseService.currentPhase$.subscribe((value) => {
+      if (value === 'START_OF_GAME') {
+        this.play('BATTLE_MUSIC');
+      }
+    })
+    this.router.events.subscribe((value) => {
+      if (value.type === EventType.NavigationStart) {
+        this.music.pause();
+        this.music = new Audio();
+      }
     })
   }
 
@@ -29,12 +59,21 @@ export class SfxService {
     this.playSfx$.next('');
   }
 
+  public toggleMute() {
+    this._mute = !this._mute;
+  }
+  public togglePause() {
+    this._pause = !this._pause;
+  }
+
   private getPath(sfxType: SfxType): Path {
     switch (sfxType) {
       case 'CLICK':
-        return BASE_PATH + 'click.ogg';
+        return UI_PATH + 'click.ogg';
       case 'SWITCH':
-        return BASE_PATH + 'switch.mp3';
+        return UI_PATH + 'switch.mp3';
+      case 'BATTLE_MUSIC':
+        return MUSIC_PATH + 'battle.wav';
     }
     return '';
   }
@@ -44,5 +83,6 @@ export class SfxService {
 export type SfxType = 
   | 'SWITCH'
   | 'CLICK'
+  | 'BATTLE_MUSIC'
   | ''
 ;
